55accc25614f935faee0433400904e3463fac207
Guilherme Afonso <guilhermeafonso208@gmail.com>
2026-01-31 02:28:57 -0300

    feat(ui): add menu with button

--- levels/main_menu/main_menu.gd
+++ levels/main_menu/main_menu.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+@onready var button = $Button
+@onready var level_1_scene = preload("res://levels/level_1/level_1.tscn")
+
+func _ready():
+	button.pressed.connect(_button_pressed)
+
+func _button_pressed():
+	get_tree().change_scene_to_packed(level_1_scene)
