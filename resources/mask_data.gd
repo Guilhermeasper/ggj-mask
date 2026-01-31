2895247c7deb54acf10414d22a643d7468c356df
Guilherme Afonso <guilhermeafonso208@gmail.com>
2026-01-31 02:29:23 -0300

    feat(data): add basic mask data

---
+++ resources/mask_data.gd
@@ -0,0 +1,20 @@
+extends Resource
+
+class_name MaskData
+
+@export_group("Identidade")
+@export var mask_name: String = "Mascara Branca"
+@export var color: Color = Color.WHITE
+@export var icon: Texture2D
+
+@export_group("Player Modifiers")
+@export var move_speed_multiplier: float = 1.0
+@export var jump_force_multiplier: float = 1.0
+@export var can_double_jump: bool = false
+@export var can_attack: bool = false
+@export var can_parry: bool = false
+
+@export_group("World & Enemy Modifiers")
+@export var enemy_aggro_range_multiplier: float = 1.0
+@export var enemy_damage_multiplier: float = 1.0
+@export var enemies_are_aggressive: bool = false
