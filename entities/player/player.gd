08ca73a833ade975f263d66f9fb6320908d3aa8b
Guilherme Afonso <gasp@cin.ufpe.br>
2026-01-31 14:43:29 -0300

    feat(player): add extra jump system

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,31 +1,35 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
-const CROUCH_SPEED = 150.0
+const SPEED := 300.0
+const JUMP_VELOCITY := -500.0
+const CROUCH_SPEED := 150.0
 
-enum MaskType {
-	WHITE = 0,
-	BLUE = 1,
-	RED = 2,
-	YELLOW = 3
-}
+const MASKS: Array[MaskData] = [
+	preload("res://resources/masks/white_mask.tres"),
+	preload("res://resources/masks/red_mask.tres"),
+	preload("res://resources/masks/yellow_mask.tres"),
+	preload("res://resources/masks/blue_mask.tres"),
+]
 
-var mask_modifier: MaskType = MaskType.WHITE
+var _current_mask: MaskData = MASKS[0]
+var _mask_index: int = 0
+var _extra_jumps_remaining: int = 0
 
-const MASK_COLORS = {
-	MaskType.WHITE: Color(1, 1, 1, 1),
-	MaskType.BLUE: Color(0.27999997, 0.13999999, 0.7, 1),
-	MaskType.RED: Color(0.7, 0.070000015, 0.070000015, 1),
-	MaskType.YELLOW: Color(0.82500005, 0.9, 0, 1)
-}
+func _ready() -> void:
+	_switch_mask(0)
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if is_on_floor():
+		_extra_jumps_remaining = _current_mask.extra_jumps
+	else:
 		velocity += get_gravity() * delta
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif _extra_jumps_remaining > 0:
+			velocity.y = JUMP_VELOCITY
+			_extra_jumps_remaining -= 1
 		
 	var direction := Input.get_axis("left", "right")
 	if direction != 0:
@@ -41,19 +45,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _switch_mask(direction: int) -> void:
-	mask_modifier = calc_new_modifier(mask_modifier, direction)
+	_mask_index = wrapi(_mask_index + direction, 0, MASKS.size())
+	_current_mask = MASKS[_mask_index]
+	_extra_jumps_remaining = _current_mask.extra_jumps
 	
-	var gradient = $Body/Mask.texture.gradient
-	var new_color = MASK_COLORS[mask_modifier]
-	for i in range(gradient.get_point_count()):
-		gradient.set_color(i, new_color)
+	# TODO: Remover quando as mascaras tiverem suas proprias texturas
+	# var gradient = $Body/Mask/Sprite2D.texture.gradient
+	# for i in range(gradient.get_point_count()):
+	# 	gradient.set_color(i, _current_mask.color)
 	
-	for i in range(4):
-		set_collision_mask_value(i + 3, i == mask_modifier || i == MaskType.WHITE)
-
-
-func calc_new_modifier(current: MaskType, direction: int) -> MaskType:
-	var new_value = (current + direction) % 4
-	if new_value < 0:
-		new_value += 4
-	return new_value as MaskType
+	for i in range(MASKS.size()):
+		set_collision_mask_value(i + 4, i == _mask_index || i == 0)
